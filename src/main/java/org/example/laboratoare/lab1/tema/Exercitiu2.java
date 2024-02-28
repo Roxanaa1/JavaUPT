3ca9081f3b2c47ace99a5cbf77447b204c93f834
Roxanaa1 <roxanaenache2003@gmail.com>
2024-02-28 23:13:43 +0200

    Lab1

--- src/main/java/org/example/laboratoare/lab1/tema/Exercitiu2.java
+++ src/main/java/org/example/laboratoare/lab1/tema/Exercitiu2.java
@@ -1,7 +1,12 @@
 package org.example.laboratoare.lab1.tema;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.util.Collections.list;
 
 public class Exercitiu2
 {
@@ -16,27 +21,39 @@ va scrie în fișierul de ieșire out.txt. Media aritmetică va fi afișată ca 
         int numarElemente = 0;
         int valoareMinima = Integer.MAX_VALUE;
         int valoareMaxima = Integer.MIN_VALUE;
-        float mediaAritmetica = suma / (float) numarElemente;
+        float mediaAritmetica=(float)suma/(float) numarElemente;
         String line;
         try {
             Scanner scanner=new Scanner(new File("src/main/resources/in.txt"));
-
+            List<Integer> list=new ArrayList();
             while(scanner.hasNext())
             {
+
                 int a=scanner.nextInt();
                 System.out.println(a);
+                list.add(a);
+
             }
+            for(int nr:list)
+            {
+                suma=suma+nr;
+            }
+            numarElemente=list.size();
+            mediaAritmetica=(float)suma/(float) numarElemente;
+            Collections.sort(list);
+            valoareMinima=list.get(0);
+            valoareMaxima=list.get(5);
+            System.out.println("Suma: " + suma);
+            System.out.println("Media aritmetică: " + mediaAritmetica);
+            System.out.println("Valoarea minimă: " + valoareMinima);
+            System.out.println("Valoarea maximă: " + valoareMaxima);
 
         }catch (IOException  e)
         {
             e.getMessage();
         }
 
-        System.out.println("Suma: " + suma);
-        System.out.println("Media aritmetică: " + mediaAritmetica);
-        System.out.println("Valoarea minimă: " + valoareMinima);
-        System.out.println("Valoarea maximă: " + valoareMaxima);
-        FileWriter writer = new FileWriter("out.txt");
+        FileWriter writer = new FileWriter("src/main/resources/out.txt");
         writer.write("Suma: " + suma + "\r\n");
         writer.write("Media aritmetică: " + mediaAritmetica + "\r\n");
         writer.write("Valoarea minimă: " + valoareMinima + "\r\n");
